68f6d133a46a1ea898c9cd16b12a2e52fc5f5ec7
Ankit <ankitshula24059918@gmail.com>
2026-03-26 16:43:11 +0000

    Fixed refresh token issue

--- apps/frontend/src/providers/AuthProvider.tsx
+++ apps/frontend/src/providers/AuthProvider.tsx
@@ -1,7 +1,28 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { refreshToken } from "@/apollo/client";
+import Loader from "@/components/Loader";
 
 export default function AuthProvider({ children }: any) {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        await refreshToken()
+      } catch (error) {
+        console.log("User not authenticated");
+      }finally{
+        setLoading(false)
+      }
+    }
+
+    initAuth()
+  }, [])
+
+  if(loading) return <Loader />
+
   return <SessionProvider>{children}</SessionProvider>;
 }
